1b39cd43dfc9a287c305738328d86cc5faceff40
Bojana-Mihajlovic <31313948+Bojana-Mihajlovic@users.noreply.github.com>
2021-11-10 21:07:45 +0100

    Add files via upload

--- kvadratna.py
+++ kvadratna.py
@@ -1,10 +1,11 @@
 import math 
 def solve_quadratic(a,b,c): 
-        if ((b*b-4*a*c)<0):
+        y = b*b-4*a*c 
+        if (y< 0):
             return None
-        x1 = (-b + math.sqrt(b*b-4*a*c))/2*a
-        x2 = (-b - math.sqrt(b*b-4*a*c))/2*a
-        if ((b*b-4*a*c)>0) and (x1!=x2): 
+        x1 = (-b + math.sqrt(y))/2*a
+        x2 = (-b - math.sqrt(y))/2*a
+        if (y>0) and (x1!=x2): 
             return (x1,x2)      
         else: 
             return (x2) 
